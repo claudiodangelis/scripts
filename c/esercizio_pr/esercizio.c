852f6c397fd2ffc42fc0a28a473c5adccfe8dcf9
Claudio d'Angelis <claudiodangelis@gmail.com>
2012-06-19 19:52:00 +0200

    finale

--- c/esercizio_pr/esercizio.c
+++ c/esercizio_pr/esercizio.c
@@ -1,3 +1,18 @@
+// Gli usuali programmi di compressione utilizzano diverse tecniche per valutare se e come un file è comprimibile.
+// Alcune di queste tecniche si basano sulla frequenza dei caratteri e sono le seguenti:
+// 
+// a) Per un carattere calcolo la sua frequenza.
+// b) Per ogni carattere calcolo quante volte il prossimo carattere è di ordine successivo a quello letto [se leggo a incontro dopo b - h incontro i ]
+// c) Per ogni carattere calcolo quante sequenze almeno lunghe 2 ci sono [per la z ho una sequenza lunga 2 in "mezzo" - aaaab -> 1 sequenza di a (non importa quante volte ripetute basta che sia una sequenza)
+// 
+// Scrivere un programma:
+// 
+// Dato un file di ingresso contenente esclusivamente i caratteri A…Z e a….z da 0…9 
+// in questo ordine ABCD…Zabcd…z012….9
+// 
+// 
+// fornire le frequente assolute e relative e percentuali.
+
 #include <stdlib.h>
 #include <stdio.h>
 
